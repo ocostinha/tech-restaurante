3740747d5ff40946d4c86f90c9ce8083c23911b1
felipecosta <fcosta.paltrinieri@gmail.com>
2024-09-24 20:50:56 -0300

    Fix build

--- src/test/java/com/fiap/tech/restaurante/RestauranteApplicationTest.java
+++ src/test/java/com/fiap/tech/restaurante/RestauranteApplicationTest.java
@@ -4,9 +4,11 @@ import io.cucumber.junit.Cucumber;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = RestauranteApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RunWith(Cucumber.class)
+@ActiveProfiles("test")
 class RestauranteApplicationTest {
 
 	@Test
